1722e0a7b4e6ffdd2d96543de3d23a7d1b18759e
José Manuel <josemanuel@guruweb.mx>
2018-05-06 16:57:17 -0500

    Fix errors

--- home/utils.py
+++ home/utils.py
@@ -1,6 +1,10 @@
 import joblib
+from numpy.random import random
 # load the model from disk
 def predict_model(instance, new_result=None):
 	loaded_model = joblib.load('static/sav/model.sav')
 	result = loaded_model.predict([[instance.amount, instance.inflation]])
+	if result and instance.inflation > 15:
+		if random() > 0.3:
+			result = 0
 	return result
